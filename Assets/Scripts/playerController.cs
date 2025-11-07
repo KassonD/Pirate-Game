c819bfbe1ece102f0631e7c4ccb21229b1a852ef
Kasson Davis <135732515+KassonD@users.noreply.github.com>
2025-11-07 15:14:34 -0600

    Updated audio control to playerController

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,25 +1,49 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
+
+public enum MovementStates
+{
+    idle,
+    idleSneak,
+    walk,
+    run,
+    sneak
+}
 
 public class playerController : MonoBehaviour
 {
     // Variables
+    public float maxHealth = 100;
     public float walkSpeed = 1;
     public float runSpeed = 2;
     public float sneakSpeed = 0.5f;
     public float friction = 1;
     public float cameraSpeed = 1;
     public Transform mainCamera;
+    public AudioClip walkAudio;
+    public AudioClip runAudio;
+    public AudioClip sneakAudio;
+    public AudioClip takeDamageAudio;
+    public AudioClip deathAudio;
 
     private Rigidbody2D rb;
+    private AudioSource audioSrc;
+    private Animator anim;
     private Vector2 moveDirection;
+    private MovementStates movementState;
+    private MovementStates prevMovementState;
     private bool isAttacking = false;
-    public int movementState = 0;  //  0-idle, 1-idle crouch, 2-walk, 3-run, 4-sneak
+    private float currentHealth;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSrc = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -31,7 +55,17 @@ public class playerController : MonoBehaviour
         // Attack
         if (Input.GetMouseButton(0))
         {
-            movementState = 5;
+            isAttacking = true;
+        }
+        else
+            isAttacking = false;
+
+        // Check for state change
+        bool stateChanged = false;
+        if (movementState != prevMovementState)
+        {
+            stateChanged = true;
+            prevMovementState = movementState;
         }
 
         // Movement states
@@ -39,21 +73,35 @@ public class playerController : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.LeftControl))
             {
-                movementState = 4;
+                movementState = MovementStates.sneak;
+
+                if (stateChanged)
+                    PlayAudio(sneakAudio, true);
             }
             else if (Input.GetKey(KeyCode.LeftShift))
             {
-                movementState = 3;
+                movementState = MovementStates.run;
+
+                if (stateChanged)
+                    PlayAudio(runAudio, true);
             }
             else
-                movementState = 2;
+            {
+                movementState = MovementStates.walk;
+
+                if (stateChanged)
+                    PlayAudio(walkAudio, true);
+            }
         }
         else if (Input.GetKey(KeyCode.LeftControl))
         {
-            movementState = 1;
+            movementState = MovementStates.idleSneak;
         }
         else
-            movementState = 0;
+        {
+            movementState = MovementStates.idle;
+            audioSrc.Stop();
+        }
 
 
     }
@@ -70,15 +118,35 @@ public class playerController : MonoBehaviour
         // Movement
         float multiplier = walkSpeed;
 
-        if (movementState == 3)
+        if (movementState == MovementStates.run)
             multiplier = runSpeed;
 
-        if (movementState == 4)
+        if (movementState == MovementStates.sneak)
             multiplier = sneakSpeed;
 
         rb.AddForce(moveDirection * multiplier);
 
         // Friction
         rb.AddForce(-rb.linearVelocity * friction);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - damage, 0, maxHealth);
+        
+        if (currentHealth == 0)
+        {
+            PlayAudio(takeDamageAudio);
+        }
+        else
+            PlayAudio(deathAudio);
+    }
+
+    private void PlayAudio(AudioClip clip, bool loop = false)
+    {
+        audioSrc.Stop();
+        audioSrc.clip = clip;
+        audioSrc.loop = loop;
+        audioSrc.Play();
     }
 }
